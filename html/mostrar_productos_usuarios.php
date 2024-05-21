b7c70ccd25a500b852b66dc9f372cea112f8eec7
sergi <sergiomogente99@gmail.com>
2024-05-21 13:16:56 +0200

    a

--- html/mostrar_productos_usuarios.php
+++ html/mostrar_productos_usuarios.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./../style.css">
     <title>Productos del Usuario</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
@@ -16,18 +16,18 @@
     <div class="cabecera">
       <div class="row">
         <div class="col">
-          <a class="navbar-brand" href="./index.php">Couture<span>App</span></a>
+          <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
         </div>
         <div class="col">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./html/pagina-usuario.php"><i class="bi bi-person-circle"></i></a>
+              <a class="nav-link active" aria-current="page" href="./pagina-usuario.php"><i class="bi bi-person-circle"></i></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link nav-link-cart" href="./html/carrito.php"><i class="bi bi-cart"></i><span id="contadorCarrito" class="contador-carrito">0</span></a>
+              <a class="nav-link nav-link-cart" href="./carrito.php"><i class="bi bi-cart"></i><span id="contadorCarrito" class="contador-carrito">0</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./index.php"><i class="bi bi-house-door"></i></a>
+              <a class="nav-link" href="./../index.php"><i class="bi bi-house-door"></i></a>
             </li>
           </ul>
         </div>
@@ -73,55 +73,9 @@
 </header>
 
 <div class="container">
-    <h1>Productos del Usuario</h1>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "couture";
+  
+?>
 
-    // Crear conexión
-    $conn = new mysqli($servername, $username, $password, $database);
-
-    // Verificar conexión
-    if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
-    }
-
-    // Obtener el término de búsqueda
-    $searchTerm = $_GET['search'];
-
-    // Consulta SQL para obtener los productos según el término de búsqueda
-    $sql = "SELECT p.*, u.nom_usuari 
-            FROM producte p 
-            INNER JOIN usuario u ON p.id_usuari = u.id_usuari
-            WHERE p.nom LIKE '%$searchTerm%'";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<h2>Resultados de búsqueda para: $searchTerm</h2>";
-        echo '<div class="row">';
-        while ($row = $result->fetch_assoc()) {
-            echo "<div class='col-md-4'>";
-            echo "<div class='card mb-4'>";
-            echo "<div class='card-body'>";
-            echo "<h5 class='card-title'>" . $row['nom'] . "</h5>";
-            echo "<p class='card-text'>Precio: " . $row['preu'] . "€</p>";
-            echo "<div class='imagen' style='text-align:center;'>";
-            echo "<img src='data:image/jpeg;base64," . base64_encode($row['foto']) . "' alt='' class='img-fluid'>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-        }
-        echo '</div>';
-    } else {
-        echo "<p>No se encontraron productos para: $searchTerm</p>";
-    }
-
-    $conn->close();
-    ?>
 </div>
 
 <script>
@@ -151,5 +105,38 @@
       });
   }
 </script>
+
 </body>
+<footer>
+      <div class="background">
+        <div class="container">
+          <div class="row general">
+            <div class="col izquierda">
+              <div class="row">
+                <div class="col titulo">
+                  <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
+                </div>
+                <div class="col">
+                  <a href="./../index.php">Avisos legales</a>
+                </div>
+                <div class="col">
+                  <a href="./../index.php">Proteccion de datos</a>
+                </div>
+              </div>
+            </div>
+            <div class="col derecha">
+              <div class="row">
+                <div class="col-4">
+                  <p>Síguenos por:</p>
+                </div>
+                <div class="col-3 rrss">
+                  <a href="www.instagram.com"><i class="bi bi-instagram"></i></a>
+                  <a href="www.facebook.com"><i class="bi bi-facebook"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 </html>
