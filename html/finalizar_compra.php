67078eb04ca70de96dbd0778c7433fe5c115757d
didachilari <didachilari@gmail.com>
2024-05-23 14:43:04 +0200

    asd

--- html/finalizar_compra.php
+++ html/finalizar_compra.php
@@ -41,11 +41,110 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Finalizar Compra</title>
     <script src="https://js.stripe.com/v3/"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../style.css">
 </head>
-<body>
-    <h1>Finalizar Compra</h1>
-    <button id="checkout-button">Pagar con Stripe</button>
+<body class="finalizar-compra">
+<header>
+  <div class="container">
+    <div class="cabecera">
+      <div class="row">
+        <div class="col">
+          <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
+        </div>
+        <div class="col">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="./pagina-usuario.php"><i class="bi bi-person-circle"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-link-cart" href="./carrito.php"><i class="bi bi-cart"></i><span id="contadorCarrito" class="contador-carrito">0</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./../index.php"><i class="bi bi-house-door"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex-mobile">
+    
+  </div>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="buscador">
+        <form class="d-flex" role="search" action="resultados_busqueda.php" method="GET" onsubmit="return validar()">
+          <button class="btn" type="submit"><i class="bi bi-search"></i></button>
+          <input class="form-control" type="search" placeholder="Buscar" aria-label="Search" name="search" id="searchInput">        </form>
+      </div>
 
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="./camisa.php">Camisa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./camiseta.php">Camiseta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./pantalon.php">Pantalon</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./chaquetas.php">Chaquetas</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./calzado.php">Calzado</i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+    <div class="container">
+        <div class="main mb-4">
+            <h1>Finalizar Compra</h1>
+            <button id="checkout-button" class="btn">Pagar con Stripe</button>
+        </div>
+    </div>
+<footer>
+  <div class="background">
+    <div class="container">
+      <div class="row general">
+        <div class="col izquierda">
+          <div class="row">
+            <div class="col titulo">
+              <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
+            </div>
+            <div class="col">
+              <a href="./../index.php">Avisos legales</a>
+            </div>
+            <div class="col">
+              <a href="./../index.php">Proteccion de datos</a>
+            </div>
+          </div>
+        </div>
+        <div class="col derecha">
+          <div class="row">
+            <div class="col-4">
+              <p>Síguenos por:</p>
+            </div>
+            <div class="col-3 rrss">
+              <a href="https://www.instagram.com"><i class="bi bi-instagram"></i></a>
+              <a href="https://www.facebook.com"><i class="bi bi-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
     <script type="text/javascript">
         var stripe = Stripe('pk_test_51PEn8fF6jOXWWkOuAqgNaFW41ZqbUIu5EiSSpYj6oLdSrYJmGohr6ngrpVP83Hd4h9JP8nXV5Zu7sLFcmv0YEpf200a2AZx2SJ');
         var checkoutButton = document.getElementById('checkout-button');
--- html/mostrar_productos_usuarios.php
+++ html/mostrar_productos_usuarios.php
@@ -70,7 +70,7 @@
     </div>
   </nav>
 </header>
-<div class="container">
+<div class="container mb-5">
   
   <h2>Busquedas de usuario</h2>
   <?php
@@ -88,33 +88,60 @@
         $userId = $row_user_id['id_usuari'];
 
         // Consulta SQL para obtener los productos del usuario según el ID de usuario
-        $sql_products = "SELECT p.*, u.nom_usuari 
-                         FROM producte p 
-                         INNER JOIN usuario u ON p.id_usuari = u.id_usuari
-                         WHERE p.id_usuari = $userId";
-        
+       $sql_products = "SELECT p.*, u.nom_usuari, m.nom AS nom_marca
+          FROM producte p 
+          INNER JOIN usuario u ON p.id_usuari = u.id_usuari
+          INNER JOIN marcas m ON p.id_marcas = m.id_marcas
+          WHERE p.id_usuari = $userId";
         $result_products = $conn->query($sql_products);
 
-        if ($result_products->num_rows > 0) {
-            echo "<h2>Productos de $searchTerm</h2>";
-            echo '<div class="row">';
-            while ($row_product = $result_products->fetch_assoc()) {
-                echo "<div class='col-md-4'>";
-                echo "<div class='card mb-4'>";
-                echo "<div class='card-body'>";
-                echo "<h5 class='card-title'>" . $row_product['nom'] . "</h5>";
-                echo "<p class='card-text'>Precio: " . $row_product['preu'] . "€</p>";
-                echo "<div class='imagen' style='text-align:center;'>";
-                echo "<img src='data:image/jpeg;base64," . base64_encode($row_product['foto']) . "' alt='' class='img-fluid'>";
-                echo "</div>";
-                echo "</div>";
-                echo "</div>";
-                echo "</div>";
-            }
-            echo '</div>';
-        } else {
-            echo "<p>No se encontraron productos para el usuario: $searchTerm</p>";
-        }
+        if ($result_products->num_rows > 0) {?>
+          <div class="row">
+          <?php while ($row = $result_products->fetch_assoc()) {?>
+            <div class="col-lg-3 col-md-4">
+              <div class="contenedor-articulo">
+                    <div class="usuario">
+                        <img src="./../img/user-line.svg" alt="">
+                        <span class="n-usuario"><?php echo $row["nom_usuari"]; ?></span>
+                    </div>
+
+                    <!-- Añadir me_gusta -->
+                    <?php include './../functions/db_connection.php'; ?>
+                    <button type="button" class="boton-corazon" data-id="<?php echo $row['id_producte']; ?>" onclick="me_gusta(<?php echo $row['id_producte']; ?>)">
+                      <img src="./../img/heart.svg" alt="">
+                    </button>
+                    <div class="imagen" style="text-align:center;">
+                            <a href="./html/detalle_producto.php?id=<?php echo $row['id_producte']; ?>">
+                                <img src="data:image/jpeg;base64,<?php echo base64_encode($row['foto']); ?>" alt="">
+                            </a>
+                        </div>
+                    <div class="contenido">
+                        <div class="row con-icon">
+                            <div class="col-10">
+                                <div class="c-1">
+                                    <p><?php echo $row["nom"]; ?></p>
+                                    <p><span>Marca:</span> <?php echo $row["nom_marca"]; ?></p>
+                                    <p><span>Precio:</span> <?php echo $row["preu"]; ?>€</p>
+                                </div>
+                            </div>
+                            <div class="col-2 carro">
+                                <div class="carrito">
+                                    <div class="row h-b">
+                                        <button type="button" class="boton-carro" onclick="agregarAlCarrito(<?php echo $row['id_producte']; ?>)">
+                                            <img src="./../img/bag.svg" alt="">
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+          <?php } ?>
+          </div>
+      <?php } else {
+          echo '<p>No se encontraron resultados</p>';
+      }
     } else {
         echo "<p>No se encontró ningún usuario con el nombre: $searchTerm</p>";
     }
