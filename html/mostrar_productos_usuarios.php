0a33d60eb13f0ff3317a6649615da2473d212394
sergi <sergiomogente99@gmail.com>
2024-05-24 20:11:06 +0200

    e

--- html/mostrar_productos_usuarios.php
+++ html/mostrar_productos_usuarios.php
@@ -34,17 +34,18 @@
       </div>
     </div>
   </div>
-  <div class="flex-mobile"></div>
+  <div class="flex-mobile">
+    
+  </div>
   <nav class="navbar navbar-expand-lg">
     <div class="container">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="buscador">
-        <form class="d-flex" role="search" action="mostrar_productos_usuarios.php" method="GET" onsubmit="return validar()">
+        <form class="d-flex" role="search" action="resultados_busqueda.php" method="GET" onsubmit="return validar()">
           <button class="btn" type="submit"><i class="bi bi-search"></i></button>
-          <input class="form-control" type="search" placeholder="Buscar" aria-label="Search" name="search" id="searchInput">        
-        </form>
+          <input class="form-control" type="search" placeholder="Buscar" aria-label="Search" name="search" id="searchInput">        </form>
       </div>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -71,148 +72,144 @@
 </header>
 <div class="container mb-5">
   
-  <h2>Resultados de búsqueda de usuario</h2>
+  <h2>Busquedas de usuario</h2>
   <?php
     include "./../functions/db_connection.php";
 
-    //farem la verificació
-    if(isset($_GET['search'])) {
-        //obtenim el que em buscat per el search
-        $searchTerm = $_GET['search'];
+    // Obtener el término de búsqueda del nombre de usuario desde la URL
+    $searchTerm = $_GET['search'];
 
-        //farem una consulta a la bbdd per obtenir el id de l'usuari del nom d'usuari buscat
-        $sql_user_id = "SELECT id_usuari FROM usuario WHERE nom_usuari LIKE '%$searchTerm%'";
-        $result_user_id = $conn->query($sql_user_id);
+    // Consulta SQL para obtener el ID del usuario basado en el nombre de usuario buscado
+    $sql_user_id = "SELECT id_usuari FROM usuario WHERE nom_usuari LIKE '%$searchTerm%'";
+    $result_user_id = $conn->query($sql_user_id);
 
-        if ($result_user_id->num_rows > 0) {
-            $row_user_id = $result_user_id->fetch_assoc();
-            $userId = $row_user_id['id_usuari'];
+    if ($result_user_id->num_rows > 0) {
+        $row_user_id = $result_user_id->fetch_assoc();
+        $userId = $row_user_id['id_usuari'];
 
-            //farem una consulta a la bbdd per obtenir els productes de l'usuari que em buscat per search
-            $sql_products = "SELECT p.*, u.nom_usuari, m.nom AS nom_marca
-                FROM producte p 
-                INNER JOIN usuario u ON p.id_usuari = u.id_usuari
-                INNER JOIN marcas m ON p.id_marcas = m.id_marcas
-                WHERE p.id_usuari = $userId";
-            $result_products = $conn->query($sql_products);
+        // Consulta SQL para obtener los productos del usuario según el ID de usuario
+       $sql_products = "SELECT p.*, u.nom_usuari, m.nom AS nom_marca
+          FROM producte p 
+          INNER JOIN usuario u ON p.id_usuari = u.id_usuari
+          INNER JOIN marcas m ON p.id_marcas = m.id_marcas
+          WHERE p.id_usuari = $userId";
+        $result_products = $conn->query($sql_products);
 
-            if ($result_products->num_rows > 0) {?>
-                <div class="row">
-                <?php while ($row = $result_products->fetch_assoc()) {?>
-              <div class="col-lg-3 col-md-4">
-                  <div class="contenedor-articulo">
-                      <div class="usuario">
-                          <img src="./../img/user-line.svg" alt="">
-                          <span class="n-usuario"><?php echo $row["nom_usuari"]; ?></span>
-                      </div>
+        if ($result_products->num_rows > 0) {?>
+          <div class="row">
+          <?php while ($row = $result_products->fetch_assoc()) {?>
+            <div class="col-lg-3 col-md-4">
+              <div class="contenedor-articulo">
+                    <div class="usuario">
+                        <img src="./../img/user-line.svg" alt="">
+                        <span class="n-usuario"><?php echo $row["nom_usuari"]; ?></span>
+                    </div>
 
-                      <!-- Añadir me_gusta -->
-                      <?php include './../functions/db_connection.php'; ?>
-                      <button type="button" class="boton-corazon" data-id="<?php echo $row['id_producte']; ?>" onclick="me_gusta(<?php echo $row['id_producte']; ?>)">
-                          <img src="./../img/heart.svg" alt="">
-                      </button>
-                      <div class="imagen" style="text-align:center;">
-                          <a href="./html/detalle_producto.php?id=<?php echo $row['id_producte']; ?>">
-                              <img src="data:image/jpeg;base64,<?php echo base64_encode($row['foto']); ?>" alt="">
-                          </a>
-                      </div>
-                      <div class="contenido">
-                          <div class="row con-icon">
-                              <div class="col-10">
-                                  <div class="c-1">
-                                      <p><?php echo $row["nom"]; ?></p>
-                                      <p><span>Marca:</span> <?php echo $row["nom_marca"]; ?></p>
-                                      <p><span>Precio:</span> <?php echo $row["preu"]; ?>€</p>
-                                  </div>
-                              </div>
-                              <div class="col-2 carro">
-                                  <div class="carrito">
-                                      <div class="row h-b">
-                                          <button type="button" class="boton-carro" onclick="agregarAlCarrito(<?php echo $row['id_producte']; ?>)">
-                                              <img src="./../img/bag.svg" alt="">
-                                          </button>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
+                    <!-- Añadir me_gusta -->
+                    <?php include './../functions/db_connection.php'; ?>
+                    <button type="button" class="boton-corazon" data-id="<?php echo $row['id_producte']; ?>" onclick="me_gusta(<?php echo $row['id_producte']; ?>)">
+                      <img src="./../img/heart.svg" alt="">
+                    </button>
+                    <div class="imagen" style="text-align:center;">
+                            <a href="./html/detalle_producto.php?id=<?php echo $row['id_producte']; ?>">
+                                <img src="data:image/jpeg;base64,<?php echo base64_encode($row['foto']); ?>" alt="">
+                            </a>
+                        </div>
+                    <div class="contenido">
+                        <div class="row con-icon">
+                            <div class="col-10">
+                                <div class="c-1">
+                                    <p><?php echo $row["nom"]; ?></p>
+                                    <p><span>Marca:</span> <?php echo $row["nom_marca"]; ?></p>
+                                    <p><span>Precio:</span> <?php echo $row["preu"]; ?>€</p>
+                                </div>
+                            </div>
+                            <div class="col-2 carro">
+                                <div class="carrito">
+                                    <div class="row h-b">
+                                        <button type="button" class="boton-carro" onclick="agregarAlCarrito(<?php echo $row['id_producte']; ?>)">
+                                            <img src="./../img/bag.svg" alt="">
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </div>
           <?php } ?>
           </div>
       <?php } else {
-          echo '<p>No se encontraron productos para este usuario.</p>';
+          echo '<p>No se encontraron resultados</p>';
       }
-  } else {
-      echo "<p>No se encontró ningún usuario con el nombre: $searchTerm</p>";
-  }
-} else {
-  echo "<p>No se ha especificado ningún término de búsqueda.</p>";
-}
+    } else {
+        echo "<p>No se encontró ningún usuario con el nombre: $searchTerm</p>";
+    }
 
-$conn->close();
-?>
+    $conn->close();
+    ?>
 
 </div>
 
 <script>
-  //el contador s'inicialitza en valor 0 i se va incrementant el numero cada cop que l'usuari afegeix un producte al carrito
-  var contadorCarrito = 0;
-  //aquesta funció el que fa es que cada cop que l'usuari li doni al botó doncs el contador del carrito sumi i es mostri el producte afegit al carrito
-  function agregarAlCarrito(idProducto) {
-      contadorCarrito++;
-      document.getElementById("contadorCarrito").textContent = contadorCarrito;
-      $.ajax({
-        url: './../functions/agregar_al_carrito.php',
-        type: 'POST',
-        data: { id: idProducto },
-        success: function(response) {
+    function validar() {
+        var searchInput = document.getElementById('searchInput').value;
+        if (searchInput.trim() === "") {
+            return false;
         }
-      });
-  }
+        return true;
+    }
 </script>
 <script>
-//aquesta funció el que fa es que si el buscador no li pasem ningun parametre doncs que es quedi en aquesta pàgina sino que mostri el resultat de la busqueda feta 
-function validar() {
-    var searchInput = document.getElementById('searchInput').value;
-    if (searchInput.trim() === "") {
-        return false;
-    }
-    return true;
-}
+  var contadorCarrito = 0;
+
+  function agregarAlCarrito(idProducto) {
+      // Incrementa el contador
+      contadorCarrito++;
+      // Actualiza la interfaz
+      document.getElementById("contadorCarrito").textContent = contadorCarrito;
+      $.ajax({
+          url: 'agregar_al_carrito.php',
+          type: 'POST',
+          data: { id: idProducto },
+          success: function(response) {
+              // Manejo de la respuesta si es necesario
+          }
+      });
+  }
 </script>
 
 </body>
 <footer>
-    <div class="background">
-      <div class="container">
-        <div class="row general">
-          <div class="col izquierda">
-            <div class="row">
-              <div class="col titulo">
-                <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
-              </div>
-              <div class="col">
-                <a href="./../index.php">Avisos legales</a>
-              </div>
-              <div class="col">
-                <a href="./../index.php">Proteccion de datos</a>
+      <div class="background">
+        <div class="container">
+          <div class="row general">
+            <div class="col izquierda">
+              <div class="row">
+                <div class="col titulo">
+                  <a class="navbar-brand" href="./../index.php">Couture<span>App</span></a>
+                </div>
+                <div class="col">
+                  <a href="./../index.php">Avisos legales</a>
+                </div>
+                <div class="col">
+                  <a href="./../index.php">Proteccion de datos</a>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col derecha">
-            <div class="row">
-              <div class="col-4">
-                <p>Síguenos por:</p>
-              </div>
-              <div class="col-3 rrss">
-                <a href="www.instagram.com"><i class="bi bi-instagram"></i></a>
-                <a href="www.facebook.com"><i class="bi bi-facebook"></i></a>
+            <div class="col derecha">
+              <div class="row">
+                <div class="col-4">
+                  <p>Síguenos por:</p>
+                </div>
+                <div class="col-3 rrss">
+                  <a href="www.instagram.com"><i class="bi bi-instagram"></i></a>
+                  <a href="www.facebook.com"><i class="bi bi-facebook"></i></a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
 </html>
